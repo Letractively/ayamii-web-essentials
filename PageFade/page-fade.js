aeebe91bcf76b56e09af39c15d5e3304e9a4e64f
tomsjunk123 <tomsjunk123@gmail.com>
2013-11-29 20:30:52 +0000

    edited page fade script

--- PageFade/page-fade.js
+++ PageFade/page-fade.js
@@ -1,4 +1,8 @@
 /** 
+    This script file is made by Tom Schillemand from Ayamii Development.
+    This file is licened under the MIT licence wich let's you free in what you want to do woth thius file
+    PLEASE be kind and leave this section in while distributing to others.
+
     This script will smoothly fade in the content as the page loads and smoothly fade back out again if the page unloads.
     As you can see it is an realy easy piece of script and can be expanded as needed. 
     
@@ -8,4 +12,3 @@
 (function() {
     $("body").hide().fadeIn(1375);   
 }
-    
